2efd6c437ce1e3b65dcc2ec30de00b9c50da42c8
ChineseLabor <130862811+ChineseLabor@users.noreply.github.com>
2023-04-27 08:43:48 -0400

    Updating our Stick.js
    Nothing much really just a little bit of changing the console effect.

--- Api/Stick.js
+++ Api/Stick.js
@@ -3,9 +3,10 @@ const Application = Express();
 const Port = process.env.PORT || 4000;
 
 Application.post("/api", ( RequestHandler, Response ) => {
-    console.warn("Caught, " + Response)
-})
+    console.warn("Caught, " + Response);
+});
 
 Application.listen( Port, () => {
-    console.log("%cApi", "background: red", "Burnt in flames with, " + Port + ".")
-})
+    /* May be subject to change. */
+    console.log("Api", " – We're following commands.");
+});
